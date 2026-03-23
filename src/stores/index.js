06285172225dd43646925414541d076a41c52178
santiagodiaz21 <fsantiagodiazc21@gmail.com>
2026-03-23 16:24:25 -0500

    Cambio de puertos

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: '186.154.204.141:20128'
+    baseURL: 'http://186.154.204.141:20128'
    //baseURL: 'http://localhost:3000'
 })
 
